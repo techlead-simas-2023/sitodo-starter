8e4575a12342501cad20710517ae45171161190a
Daya Adianto <addianto@users.noreply.github.com>
2022-11-11 13:57:44 +0700

    Deliberately break the code (#21) (#23)
    Closes #21.

--- src/main/java/com/example/sitodo/service/impl/MotivationMessageServiceImpl.java
+++ src/main/java/com/example/sitodo/service/impl/MotivationMessageServiceImpl.java
@@ -43,7 +43,7 @@ public class MotivationMessageServiceImpl implements MotivationMessageService {
         String output = "";
 
         if (total == 0) {
-            output += emptyListMessage;
+            output += "";
         } else if (total < manyItemsThreshold) {
             output += fewItemsMessage;
 
