35df3a515a68886bb24891ab6750df1ea25e67d9
Daya Adianto <addianto@users.noreply.github.com>
2023-12-12 10:39:49 +0700

    Prepare starter code & training instructions

--- src/main/java/com/example/sitodo/controller/MainController.java
+++ src/main/java/com/example/sitodo/controller/MainController.java
@@ -2,11 +2,13 @@ package com.example.sitodo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String showMainPage() {
         return "redirect:/list";
     }
